9e23126524d59dbf082f795a1cbe7ce9549858de
Matheus Lima <matheusslimabm@gmail.com>
2024-08-28 19:59:13 -0300

    ediçoes finais

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,7 @@ module.exports = {
                     presets: ['@babel/env']
                 }
             }
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
+        }] 
     },
     devtool: 'source-map'
 }
